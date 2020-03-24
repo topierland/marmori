ddcea4a16d644846299bd0bd5954abbe3165b51f
Topi Särkiniemi <topi.sarkiniemi@gmail.com>
2020-03-24 19:13:37 +0200

    Added js file

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+// Smooth scroll to content
+const $root = $('html, body');
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
